7fd6c8e05adf6c3e810f2fcc489fcb725b6dab84
sunkusaiabhinav <saiabhinav251090@gmail.com>
2026-08-14 21:45:28 +0530

    Update API title from feature

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 
-app = FastAPI(title="Task Manager main Feature API")
+app = FastAPI(title="Task Manager FEATURE VERSION")
 
 
 class TaskCreate(BaseModel):
